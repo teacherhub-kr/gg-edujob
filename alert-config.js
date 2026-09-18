103ea62b0fc8374f16176507ff9a8953f38061e9
teacherhub-kr <puzzlex5@gmail.com>
2026-09-18 23:08:28 +0900

    Enable live Edujob Web Push backend

--- alert-config.js
+++ alert-config.js
@@ -1,5 +1,5 @@
 window.EDUJOB_ALERT_CONFIG=Object.freeze({
-  enabled:false,
-  endpoint:'',
-  vapidPublicKey:''
+  enabled:true,
+  endpoint:'https://ahghkusvbfwrdmrhkgwe.supabase.co/functions/v1/push-subscription',
+  vapidPublicKey:'BIzLo7Ri325DU7t4-FUQ3T1TMRgRGn7iI8mw875ZpEHIHniQXOoUMRGHtoBCNknlcFUdFEx9i6mTlul8KVpBh1U'
 });
